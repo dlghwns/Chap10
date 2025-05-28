a843db409dccd6558b617ea616715c1d10e70831
user <user@DESKTOP-VMLPVUF>
2025-05-28 16:31:10 +0900

    초기 커밋.

---
+++ Project1/contact.h
@@ -0,0 +1,26 @@
+//#pragma once
+
+#ifndef _CONTACT_H_1234123
+#define _CONTACT_H_1234123
+
+enum gender { MAN, WOMAN };
+typedef enum gender EGnder;
+
+struct contact
+{
+	char name[24];
+	char phone[24];
+	EGnder gender;
+	int year;
+};
+typedef struct contact Contact;
+
+
+
+int isEqualContact(Contact a, Contact b);
+
+void printContact(Contact ct);
+
+
+
+#endif
---
+++ Project1/main.c
@@ -0,0 +1,43 @@
+
+#define _CRT_SECUIRE_NO_WARNINGS
+#include <stdio.h>
+#include "contact.h"
+
+void testContact();
+
+int main()
+{
+	testContact();
+
+
+	return 0;
+}
+
+
+void testContact()
+{
+	Contact c1 = {
+		"È«±æµ¿",
+		"01027780282",
+		MAN,
+		2025
+	};
+
+
+	Contact c2 = {
+		"È«±æµ¿",
+		"01027780282",
+		MAN,
+		2025
+	};
+
+	Contact c3 = {
+		"±è±æµ¿",
+		"01027780282",
+		MAN,
+		2025
+	};
+
+
+	printContact(c1);
+}
